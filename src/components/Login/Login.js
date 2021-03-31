dfbe982c20e8aeeeb1231049feefb4d2cb262ca8
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-03-31 19:51:54 +0530

    added login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import loginImage from '../../images/login.svg';
+import './Login.css';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className='login-container'>
+      <img className='image-container' src={loginImage} />
+      <form className='form-container'>
+        <h2>Login</h2>
+        <label for='name'>Name</label>
+        <input id='name' type='text' placeholder='Your Name' />
+        <label for='email'>Email</label>
+        <input id='email' type='email' placeholder='abc@def.com' />
+        <button type='submit' className='login-btn'>
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
