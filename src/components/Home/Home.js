9547d6b60576220606ec05de3978c9d6910d2aea
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-04-01 12:34:55 +0530

    added filters

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,18 +2,35 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Header from '../Header/Header';
 import Product from '../Product/Product';
+import './Home.css';
 
 const Home = () => {
   const products = useSelector((state) => state.products);
   let productsArr = [];
+  console.log(products);
 
   for (let product of Object.keys(products)) {
     productsArr.push(products[product]);
   }
 
+  const handleFilter = () => {
+    productsArr.sort((a, b) => a - b);
+  };
+
+  console.log(productsArr);
+
   return (
     <>
       <Header />
+      <div className='filter-container'>
+        Filter By:
+        <span className='price-filter' onClick={handleFilter}>
+          Price
+        </span>
+        <span className='quantity-filter' onClick={handleFilter}>
+          Quantity
+        </span>
+      </div>
       {productsArr.map((product, index) => (
         <Product key={index} product={product} />
       ))}
